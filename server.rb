7a7219e18dc45354f807374fb19a988e11503e8e
Ariel <poonsaucey@comcast.net>
2015-06-30 15:55:41 -0400

    file edit

--- server.rb
+++ server.rb
@@ -6,6 +6,11 @@ get '/'  do
 Este es mi primer website, aqui puede formatear lo que sea y ponerlo en este website que me hiso el sistema"
 end
 
+get '/facebook' do 
+	"Sup facebook.friends!
+	This is a test of my first website   =)"
+end
+
 # get '/blah' do
 #   "Hello World"
 # end
