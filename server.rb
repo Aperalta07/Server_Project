671cb9de9b5b67521543a5ade31afaa0b51e893c
Ariel <poonsaucey@comcast.net>
2015-06-30 15:51:47 -0400

    file edit

--- server.rb
+++ server.rb
@@ -2,9 +2,8 @@
 require 'sinatra'
 
 get '/'  do
-  "Hola Familia"
-  "Espero que esten bien"
-  "Este es mi primer website, aqui puede formatear lo que sea y ponerlo en este website que me hiso el sistema"
+  "Hola Familia! 
+Este es mi primer website, aqui puede formatear lo que sea y ponerlo en este website que me hiso el sistema"
 end
 
 # get '/blah' do
