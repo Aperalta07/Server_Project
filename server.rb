602b7fb7fe25fb0d442c9f0462a9754ca217473c
Ariel <poonsaucey@comcast.net>
2015-06-30 12:30:10 -0400

    heroku

--- server.rb
+++ server.rb
@@ -1,10 +1,10 @@
 
 require 'sinatra'
 
-get '/'  do
-  "Hello world"
-end
+# get '/'  do
+#   "Hello world"
+# end
 
-get '/blah' do
-  "Hello World"
-end
+# get '/blah' do
+#   "Hello World"
+# end
